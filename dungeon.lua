4a57cfefd2b63e4f15d3f2a3bcb11f36efaf7f61
M1st <56798290+M1stHub@users.noreply.github.com>
2026-04-18 01:59:55 +0200

    Update dungeon.lua

--- dungeon.lua
+++ dungeon.lua
@@ -4,17 +4,6 @@ local cfg         = getgenv().DungeonConfig or {}
 local LocalPlayer = game:GetService("Players").LocalPlayer
 
 local function mainScript()
-    getgenv().Config = {
-        ["Auto Attack Dungeon"]      = true,
-        ["Select Weapon Dungeon"]    = "Melee",
-        ["Auto Store Fruit"]         = true,
-        ["Random Devil Fruit"]       = true,
-        ["Auto Turn On V4"]          = true,
-        ["Auto Turn On Observation"] = true,
-        ["Auto Turn On Buso"]        = true,
-        ["Auto Turn On V3"]          = true,
-        ["Boost Fps"]                = true,
-    }
     getgenv().Key = cfg.key or ""
     loadstring(game:HttpGet(cfg.scriptUrl))()
 end
